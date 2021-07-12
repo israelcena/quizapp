ccd0f0bc17560cdd847fdde1e5c1e9f67f321d47
Israel Cena <israelsjm@gmail.com>
2021-07-12 03:13:31 -0300

    include questions

---
+++ script.js
@@ -0,0 +1,35 @@
+const quisDate = [
+  {
+    question: 'Quem descobriu o Brasil ?',
+    a: 'Cristovão Colombo',
+    b: 'Dom Pedro I',
+    c: 'Dom João IV',
+    d: 'Pedro Alves Cabral',
+    correct: 'd'
+  },
+  {
+    question: 'Quem Libertou os Escravos ?',
+    a: 'Getulho Vargas',
+    b: 'Bolsonaro',
+    c: 'Princesa Isabel',
+    d: 'Joaquim José da Silva Xavier',
+    correct: 'c'
+  },
+  {
+    question: 'Em que Continente Fica Portugal ?',
+    a: 'Europa',
+    b: 'America',
+    c: 'África',
+    d: 'Japão',
+    correct: 'a'
+  },
+  {
+    question: 'Em que ano o Brasil teve sua Independência ?',
+    a: '1808',
+    b: '1888',
+    c: '1822',
+    d: '1500',
+    correct: 'c'
+  },
+  
+]
