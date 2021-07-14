58f65f2d227cd3c76fe22072b64772b1d6357c81
Israel Cena <israelsjm@gmail.com>
2021-07-14 02:43:57 -0300

    fix grammarly and added another questions

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const quizData = [
   {
     question: 'Quem descobriu o Brasil ?',
-    a: 'Cristovão Colombo',
+    a: 'Cristóvão Colombo',
     b: 'Dom Pedro I',
     c: 'Dom João IV',
-    d: 'Pedro Alves Cabral',
+    d: 'Pedro Álvares Cabral',
     correct: 'd'
   },
   {
     question: 'Quem Libertou os Escravos ?',
-    a: 'Getulho Vargas',
-    b: 'Bolsonaro',
+    a: 'Getúlio Vargas',
+    b: 'Jair M. Bolsonaro',
     c: 'Princesa Isabel',
     d: 'Joaquim José da Silva Xavier',
     correct: 'c'
@@ -31,9 +31,64 @@ const quizData = [
     d: '1500',
     correct: 'c'
   },
-
+  {
+    question: 'Em que ano foi assinada a Lei Áurea ?',
+    a: '1822',
+    b: '1888',
+    c: '1808',
+    d: '1912',
+    correct: 'b'
+  },
+  {
+    question: 'Quem foi o primeiro Rei Brasil ?',
+    a: 'Dom Pedro I',
+    b: 'Pedro Álvares Cabral',
+    c: 'Dom João IV',
+    d: 'Dom João III',
+    correct: 'a'
+  },
+  {
+    question: 'Em que ano o Brasil foi pentacampeão mundial de futebol?',
+    a: '1994',
+    b: '1998',
+    c: '2002',
+    d: '2012',
+    correct: 'c'
+  },
+  {
+    question: 'Quem foi o primeiro presidente do Brasil eleito pelo voto popular ?',
+    a: 'Floriano Peixoto',
+    b: 'Prudente de Morais',
+    c: 'Marechal Deodoro da Fonseca',
+    d: 'Getúlio Vargas',
+    correct: 'b'
+  },
+  {
+    question: 'Em que ano o Brasil foi descoberto ?',
+    a: '1822',
+    b: '1515',
+    c: '1495',
+    d: '1500',
+    correct: 'd'
+  },
+  {
+    question: 'Qual Presidente Criou a Petrobrás ?',
+    a: 'Eurico Gaspar Dutra',
+    b: 'Prudente de Morais',
+    c: 'Juscelino Kubitschek de Oliveira',
+    d: 'Getúlio Vargas',
+    correct: 'd'
+  },
+  {
+    question: 'Em que ano o Brasil se tornou uma República ?',
+    a: '1889',
+    b: '1898',
+    c: '1822',
+    d: '1888',
+    correct: 'a'
+  },
 ];
-let currentQuiz = 0;
+let currentQuiz = undefined;
 /* Options El */
 const questionEl = document.getElementById('question');
 const commentEl = document.getElementById('comment');
@@ -46,7 +101,7 @@ const submitBtnEl = document.getElementById('button');
 const radiusEl = document.querySelectorAll('input')
 
 function initialQuiz() {
-  questionEl.innerHTML = 'Bem Vindo Ao Gerador de Quiz !';
+  questionEl.innerHTML = 'Bem Vindo Ao Quiz App!';
   aOptionEl.innerHTML = '';
   bOptionEl.innerHTML = 'Clique em iniciar para começar';
   cOptionEl.innerHTML = '';
